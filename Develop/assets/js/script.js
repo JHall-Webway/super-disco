daf2189f9184367a9315440bad05f0340bffe767
JHallUofA <83625694+JHallUofA@users.noreply.github.com>
2021-07-17 13:17:04 -0700

    added persistance

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,57 +1,93 @@
-function updateTime(time, task) {
-    $("#currentDay").text(time.format("DD/MM/YYYY hh:mm:ss"));
+var tasks = []
+//Assigning moments to time blocks
+$(".hour").each(function (index) {
+    $(this).text(moment(6, "HH00")
+        .add(index, "hours")
+        .format("HH00")
+    )
+})
+//Loads tasks into time blocks
+var loadTasks = function () {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    if (!tasks) {
+        tasks = []
+    }
     $(".row").each(function (index) {
-       var timeID = parseInt($(this)
+        for (let i = 0; i < tasks.length; i++) {
+            const task = tasks[i];
+            var timeID = $(this)
             .children(".hour")
             .text()
-            .split(" ")[0])
-        
-        $(this).attr("id", timeID)
+            if (task.time === timeID) {
+                $(this).children(".col-10").text(task.task)
+            }
+        }
+    })
+}
+//Saves tasks to local storage
+var saveTasks = function () {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+//Updates time blocks, loads tasks
+function updateTime(time) {
+    $("#currentDay").text(time.format("DD/MM/YYYY hh:mm:ss"));
+    $(".row").each(function (index) {
+        var timeID = parseInt($(this)
+            .children(".hour")
+            .text()
+        )
 
-       if (parseInt(time.format("H")) > timeID) {
-           $(this).children(".col-10").removeClass("past present future").addClass("past")
-       } else if (parseInt(time.format("H")) === timeID) {
-        $(this).children(".col-10").removeClass("past present future").addClass("present")
-       } else {
-        $(this).children(".col-10").removeClass("past present future").addClass("future")
-       }
+        if (parseInt(time.format("HH00")) > timeID) {
+            $(this).children(".col-10").removeClass("past present future").addClass("past")
+        } else if (parseInt(time.format("HH00")) === timeID) {
+            $(this).children(".col-10").removeClass("past present future").addClass("present")
+        } else {
+            $(this).children(".col-10").removeClass("past present future").addClass("future")
+        }
     })
 }
 //Open edit window
 $(".row").on("click", ".col-10", function () {
-   var text = $(this).text()
-   var textInput = $("<textarea>")
+    var text = $(this).text()
+    var textInput = $("<textarea>")
         .val(text)
         .addClass("col-10");
-   $(this).replaceWith(textInput);
-   textInput.trigger("focus");
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
 });
 //Save Button
 $(".row").on("click", ".fas", function () {
+
+    var rowNum = $(this)
+        .parent()
+        .parent()
+        .children(".hour")
+        .text();
+
     var saveBox = $(this)
         .parent()
         .parent()
         .children(".col-10")
-    
+
     var text = saveBox.val();
 
     var savedTask = $("<div>")
         .text(text)
         .addClass("col-10 future")
-    console.log(text)
+
+    var taskObj = {
+        task: text,
+        time: rowNum
+    }
+
+    tasks.push(taskObj);
+    saveTasks();
     saveBox.replaceWith(savedTask);
 })
 //Clock
 setInterval(function () {
     var time = moment();
-    updateTime(time, "clock");
+    updateTime(time);
 }, 1000);
-
-$(".hour").each(function (index) {
-    $(this).text(moment(6, "HH")
-        .add(index, "hours")
-        .format("H A")    
-    )
-})
-
-updateTime(moment());
+updateTime(moment(), tasks);
+loadTasks();
